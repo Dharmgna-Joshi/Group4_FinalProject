d5410f377f15a5266aac355e504a0bc690e57c01
dharmgnajoshi131199@gmail.com <dharmgnajoshi131199@gmail.com>
2024-04-11 19:39:27 -0400

    products page with drop down and displaying the data with quantity

--- Group4_FinalProject/Products.aspx
+++ Group4_FinalProject/Products.aspx
@@ -1,9 +1,49 @@
-﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="Group4_FinalProject.About" %>
+﻿<%@ Page Title="Products" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="Group4_FinalProject.Products" %>
 
-<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <main aria-labelledby="title">
-        <h2 id="title"><%: Title %>.</h2>
-        <h3>Your application description page.</h3>
-        <p>Use this area to provide additional information.</p>
-    </main>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        .boldLabel {
+            font-weight: bold;
+        }
+        .product-image {
+            max-width: 100%;
+            height: auto;
+        }
+        .product-info-container {
+            display: flex;
+            justify-content: space-between;
+        }
+        .product-details {
+            flex: 1;
+        }
+    </style>
+    <h2>Products</h2>
+
+    <div class="product-info-container">
+        <!-- Container for selecting the product -->
+        <div class="product-details">
+            <!-- Dropdown for selecting the product -->
+            <asp:DropDownList ID="ddlProducts" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlProducts_SelectedIndexChanged">
+                <asp:ListItem Text="Select a product" Value="" />
+            </asp:DropDownList>
+
+            <!-- Container for displaying the product info -->
+            <div>
+                <h3>Product Information:</h3>
+                <asp:Label ID="lblDescription" runat="server" Text="Description: " CssClass="boldLabel"></asp:Label>
+                <br />
+                <asp:Label ID="lblPrice" runat="server" Text="Price: " CssClass="boldLabel"></asp:Label>
+                  <br />
+                <!-- Text field for entering product quantity -->
+                <asp:TextBox ID="txtQuantity" runat="server" CssClass="quantity-field" TextMode="Number" Min="1" />
+                <asp:Label ID="lblQuantity" runat="server" Text="Quantity: " AssociatedControlID="txtQuantity"></asp:Label>
+                </div>
+        </div>
+        
+        <!-- Container for displaying the product image -->
+        <div>
+            <asp:Image ID="imgProduct" runat="server" CssClass="product-image" />
+        </div>
+
+    </div>
 </asp:Content>
--- Group4_FinalProject/Products.aspx.cs
+++ Group4_FinalProject/Products.aspx.cs
@@ -1,17 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 namespace Group4_FinalProject
 {
-    public partial class About : Page
+    public partial class Products : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                PopulateProductsDropdown();
+            }
         }
+        private void PopulateProductsDropdown()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ElectronicsConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT ProductID, ProductName FROM Products", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                ddlProducts.DataSource = reader;
+                ddlProducts.DataValueField = "ProductID";
+                ddlProducts.DataTextField = "ProductName";
+                ddlProducts.DataBind();
+
+                reader.Close();
+                con.Close();
+
+                ddlProducts.Items.Insert(0, new ListItem("Select a product", ""));
+            }
+        }
+
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ElectronicsConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT Description, Price, ImageUrl FROM Products WHERE ProductID = @ProductID", con);
+                cmd.Parameters.AddWithValue("@ProductID", ddlProducts.SelectedValue);
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    lblDescription.Text = "Description: " + reader["Description"].ToString();
+                    lblPrice.Text = "Price: $" + reader["Price"].ToString();
+                    imgProduct.ImageUrl = reader["ImageUrl"].ToString(); // Assuming the column name is 'ImageUrl'
+                }
+                else
+                {
+                    lblDescription.Text = "Description: Not found.";
+                    lblPrice.Text = "Price: Not found.";
+                    imgProduct.ImageUrl = ""; 
+                }
+
+                reader.Close();
+                con.Close();
+            }
+        }
+
     }
 }
